3dc4efcedffac063eb885a539220f1e5d88adf71
Ruan Pingcheng <ruanpingcheng@gmail.com>
2014-09-26 09:42:51 +0800

    Start on another exploration computer

---
+++ ExplorationSimulator/src/Model/ScanExplorationComputer.java
@@ -0,0 +1,18 @@
+package Model;
+
+public class ScanExplorationComputer extends ExplorationComputer{
+	
+	public ScanExplorationComputer(int rowCount, int colCount,
+			ExplorationEnvironment env) {
+		super(rowCount, colCount, env);
+	}
+
+	@Override
+	public Action getNextStep(Robot robot) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
